d5988efadbca272695666036f63f476743e867f9
Yusuf Abdulkarim <haywhyze@gmail.com>
2021-06-20 17:11:08 +0100

    remove initial sample page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useReducer } from "react";
-import { ToastProvider } from "react-toast-notifications";
-import "./App.css";
-import "./tailwind.generated.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ContextUser } from "./context";
-import PrivateRoute from "./auth/PrivateRoute";
-import Dashboard from "./Views/Dashboard";
-import SamplePage from "./Views/SamplePage";
-import LoginPage from "./Views/LoginPage";
-import { auth } from "./reducers/initialState";
-import userReducer from "./reducers/auth";
+import React, { useReducer } from 'react';
+import { ToastProvider } from 'react-toast-notifications';
+import './App.css';
+import './tailwind.generated.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ContextUser } from './context';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './Views/Dashboard';
+import LoginPage from './Views/LoginPage';
+import { auth } from './reducers/initialState';
+import userReducer from './reducers/auth';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 function App() {
@@ -20,14 +19,11 @@ function App() {
       <ContextUser.Provider value={[state, dispatch]}>
         <Router>
           <Switch>
-            <Route path="/about"></Route>
-            <Route path="/sample">
-              <SamplePage />
-            </Route>
-            <Route path="/login">
+            <Route path='/about'></Route>
+            <Route path='/login'>
               <LoginPage />
             </Route>
-            <PrivateRoute path="/">
+            <PrivateRoute path='/'>
               <Dashboard />
             </PrivateRoute>
           </Switch>
